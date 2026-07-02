81db74ac52c88e0b1feb8054de035ee1cbe6c0c7
priyanshupnwr <priyanshupanwarrr@gmail.com>
2026-07-02 13:32:13 +0530

    update lead service

--- src/services/leadService.js
+++ src/services/leadService.js
@@ -2,26 +2,46 @@ import axios from "axios";
 
 const API_URL = "http://localhost:5000/api/leads";
 
+// Helper function to get auth headers
+const getConfig = () => {
+  const token = localStorage.getItem("token");
+
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+};
+
 // Get All Leads
 export const getLeads = async () => {
-  const response = await axios.get(API_URL);
+  const response = await axios.get(API_URL, getConfig());
   return response.data;
 };
 
 // Create Lead
 export const createLead = async (leadData) => {
-  const response = await axios.post(API_URL, leadData);
+  const response = await axios.post(API_URL, leadData, getConfig());
   return response.data;
 };
 
 // Update Lead
 export const updateLead = async (id, leadData) => {
-  const response = await axios.put(`${API_URL}/${id}`, leadData);
+  const response = await axios.put(
+    `${API_URL}/${id}`,
+    leadData,
+    getConfig()
+  );
+
   return response.data;
 };
 
 // Delete Lead
 export const deleteLead = async (id) => {
-  const response = await axios.delete(`${API_URL}/${id}`);
+  const response = await axios.delete(
+    `${API_URL}/${id}`,
+    getConfig()
+  );
+
   return response.data;
 };
